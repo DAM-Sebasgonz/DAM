93301292a832618f6f6e2a9cde63f0ddb739e870
DAM-Sebasgonz <sebastianmoises2007@gmail.com>
2025-11-12 17:45:53 +0000

    ya esta lo de la listas

--- PRO/TEMA 2/Ejercicios tema 2/lista_personas.py
+++ PRO/TEMA 2/Ejercicios tema 2/lista_personas.py
@@ -3,7 +3,7 @@
 personas = []
 
 while True:
-    menu = "1. Insertar\n9. Salir"
+    menu = "1. Insertar \n3. Borrar usuario \n4. Mostrar datos \n9. Salir"
     print(menu)
     
     opc = input("opcion ? --> ")
@@ -12,7 +12,7 @@ while True:
             aux_persona = []
             for pers in personas:
                 nif = input("Escriba su NIF")
-                if pers[0] == nif
+                if pers[0] == nif:
                     print(f"Error...el nif {nif} ya esta en la lista")
                     break
             else: 
@@ -28,12 +28,18 @@ while True:
                     aux_persona.append(salario)
 
                     personas.append(aux_persona)
-                    
-
-
-
-
-            
+        case "3":
+            nif_borrar = input("Introduzca el NIF del usuario a borrar -->")
+            for pers in personas:
+                if pers[0] == nif_borrar:
+                    personas.remove(pers)
+                    print(f"Usuario con NIF {nif_borrar} borrado")
+                    break
+            else:
+                print(f"El NIF {nif_borrar} no se encuentra en la lista")
+        case "4":
+            for pers in personas:
+                print(f"NIF: {pers[0]} | Nombre: {pers[1]} | Trabaja: {pers[2]} | Salario: {pers[3]} ")  
         case "9":
             print("\nFin del programa")
             break
