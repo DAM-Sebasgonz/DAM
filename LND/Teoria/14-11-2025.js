f83d8f848d9d828dd0a063e65626af2a55123fad
DAM-Sebasgonz <email>
2025-11-14 08:47:58 +0000

    js

--- LND/Teoria/14-11-2025.js
+++ LND/Teoria/14-11-2025.js
@@ -1,8 +1,19 @@
-const numero1 = 5;
-const numero2 = 10;
-const numero3 = 15;
-let suma;
+// const numero1 = 5;
+// const numero2 = 10;
+// const numero3 = 15;
+// let suma;
 
-suma = numero1 + numero2;
-suma = numero1 + numero2 + numero3;
-document.write("La suma es : " + suma + "<br>")
+// suma = numero1 + numero2;
+// suma = numero1 + numero2 + numero3;
+// document.write("La suma es : " + suma + "<br>")
+
+
+let numero1 = parseFloat(prompt("Ingrese el primer numero: "))
+let numero2 = parseFloat(prompt("Ingrese el segundo numero: "))
+document.write("El primer numero ingresado es:" + numero1 + "<br>")
+document.write("El segundo numero ingresado es:" + numero2 + "<br>")
+let suma = numero1 + numero2
+
+
+
+
