5d012c81fdaab151938f5e4466dc1eec72676748
DAM-Sebasgonz <email>
2025-11-14 08:39:21 +0000

    lnd 14-11

---
+++ LND/Teoria/14-11-2025.js
@@ -0,0 +1,8 @@
+const numero1 = 5;
+const numero2 = 10;
+const numero3 = 15;
+let suma;
+
+suma = numero1 + numero2;
+suma = numero1 + numero2 + numero3;
+document.write("La suma es : " + suma + "<br>")
