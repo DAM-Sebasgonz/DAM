77d8e269eb0f0153311c14d3d8210a506f3eb7af
DAM-Sebasgonz <email>
2025-11-17 09:21:47 +0000

    hola

---
+++ PRO/TEMA 2/Diccicionario.py
@@ -0,0 +1,44 @@
+#maneras de crear un diccionario
+
+# dicc = dict()
+# dicc02 =  {}
+
+#Diccionario con elementos
+
+# dicc01 = {"a":1, "b":2, "c":3}
+
+#Ejemplo mas practico
+
+# country_capitals = {
+#     "Germany": "Berlin",
+#     "Canada" : "Ottawa",
+#     "England": "London",
+# }
+
+# for dato in country_capitals:
+#     print(f"{dato:18}")
+
+#uso del get
+
+# person = {'name': 'Phill', 'age': 22}
+# print('Name: ', person.get('name'))
+# print('Age: ', person.get('age'))
+# # clave que no está
+# print('Salary: ', person.get('salary'))
+# # se indica valor a devolver
+# print('Salary: ', person.get('salary', 0.0))
+
+# uso del in
+
+# country_capitals = {"England": "London", "Italy": "Rome"}
+# print(len(country_capitals)) # Output: 2
+
+# numbers = {10: "ten", 20: "twenty", 30: "thirty"}
+# print(len(numbers))
+
+# countries = {}
+# print(len(countries))
+
+
+
+
