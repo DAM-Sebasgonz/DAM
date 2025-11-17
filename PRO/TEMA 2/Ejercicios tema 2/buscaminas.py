0baf68e27256693c84df37f522173125a156c767
DAM-Sebasgonz <sebastianmoises2007@gmail.com>
2025-11-17 18:01:12 +0000

    buscaminas

--- PRO/TEMA 2/Ejercicios tema 2/buscaminas.py
+++ PRO/TEMA 2/Ejercicios tema 2/buscaminas.py
@@ -1,5 +1,27 @@
 import random
 
-fil = 8 
+# Esto es la cantidad de filas y de la mierda esa de columnas
+fil = 8
 col = 8
+numbomba = 10
 
+print("=" * 50) 
+print("BUSCAMINAS - Tablero") #esto lo vi en tiktok y quedo bien jajaja
+print("=" * 50)
+
+# tablero vacío de 8x8
+tablero = [[0 for _ in range(col)] for _ in range(fil)]
+
+print("\nBombas colocadas en el tablero:")
+
+bombas_colocadas = 0
+
+while bombas_colocadas <= numbomba:
+    # Que la esto seae aleatoria
+    fila = random.randint(0, fil - 1)
+    columna = random.randint(0, col - 1)
+    
+    # el esto coloca bombas en el tablero si no hay una bomba ya
+    if tablero[fila][columna] != 'B':
+        tablero[fila][columna] = 'B'
+        bombas_colocadas += 1
