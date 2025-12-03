9df5e5edddedb1f631e29bbad367756617e6992d
DAM-Sebasgonz <email>
2025-12-03 13:48:16 +0000

    funciones python y ejercicios JS

---
+++ PRO/TEMA 3/Teoria.py
@@ -0,0 +1,39 @@
+#FUNCION PYTHON SIMPLE con una suma
+
+# def sumar(a, b):
+#     resultado = a + b
+#     print (a+b)
+#     return resultado
+
+# a = 3
+# b = 4
+# valor_suma = sumar(a, b)
+
+#FUNCION QUE DEVUELVE LAS VOCALES EN LINEA UNA DEBAJO DE LA OTRA
+
+# def vocales():
+#     return"aeiou"
+
+# for vocal in vocales():
+#     print(vocal)
+
+#EJEMPLO DE FUNCION CON ENTRADA DE TECLADO
+
+# def saluda(name):
+#     print(f" Hola {name}")
+#     return
+
+# entrada = int(input("numero --> "))
+
+# if entrada == 5:
+#     saluda(name="gay")
+# else:
+#     saluda (name="hetero")
+
+    
+
+
+
+
+
+
