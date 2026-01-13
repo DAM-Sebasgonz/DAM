c0e8339086ce1a42e783bb89ea4c11c30f487363
DAM-Sebasgonz <email>
2026-01-13 12:20:58 +0000

    hola

--- PRO/TEMA 3/Ficheros/FICHEROS/09-json-leer.py
+++ PRO/TEMA 3/Ficheros/FICHEROS/09-json-leer.py
@@ -1,10 +1,10 @@
 import json
 
-with open ("files/contactos.json", "r") as jsonfile:
+with open ("C:/DAM/PRO/TEMA 3/Ficheros/FICHEROS/files/contactos.json", "r") as jsonfile:
     datos = json.load(jsonfile)
 
-    # datos es una fila donde cada elemento es un diccionario
+    # datos es una lista donde cada elemento es un diccionario
 
     for fila in datos:
         print(fila) # en orden inverso
-        print(f'\tnombre: {fila["nombre"]}\n\templeo: {fila["empleo"]} \n\tcorreo:{fila["email"]}')
+        # print(f'\tnombre: {fila["nombre"]}\n\templeo: {fila["empleo"]} \n\tcorreo:{fila["email"]}')
