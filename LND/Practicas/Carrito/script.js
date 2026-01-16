a107ffe8f5883b07352a758564773cd2419d3bef
DAM-Sebasgonz <email>
2026-01-16 08:56:44 +0000

    index

---
+++ LND/Practicas/Carrito/script.js
@@ -0,0 +1,34 @@
+// Son objetos que tengan nombre precio y cantidad
+// Una lista de estos objetos(3)
+// Funcion que nos muestre todos lo objetos de la lista
+//Funcion que calcule el total del importe del carrito
+//document.write para mostrar
+
+let monitor = {
+    nombre: "Asus TUF Gaming",
+    precio: "120€",
+    cantidad : "10"
+}
+
+let raton = {
+    nombre: "Logitech G203",
+    precio: "30€",
+    cantidad : "50"
+}
+
+let teclado ={
+    nombre:  "EpoMaker TH80",
+    precio: "80€",
+    cantidad : "30"
+}
+
+let lista_objetos = []
+
+function mostrar_objetos (){
+    lista_objeto
+}
+
+
+
+
+
