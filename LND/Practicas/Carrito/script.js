b3e831495406ee23ec5d9756057555a37390343d
DAM-Sebasgonz <email>
2026-01-21 13:39:11 +0000

    nuevo

--- LND/Practicas/Carrito/script.js
+++
@@ -1,6 +0,0 @@
-// Son objetos que tengan nombre precio y cantidad
-// Una lista de estos objetos(3)
-// Funcion que nos muestre todos lo objetos de la lista
-//Funcion que calcule el total del importe del carrito
-//document.write para mostrar
-
