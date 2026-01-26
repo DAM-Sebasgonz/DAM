26b4724d61524979140b877f366066297c7605bf
DAM-Sebasgonz <email>
2026-01-26 11:33:46 +0000

    js

--- LND/Ejemplo/Carrito/script.js
+++ LND/Ejemplo/Carrito/script.js
@@ -31,7 +31,7 @@ function anadirCarrito(nombre, precio){
 //Funcion para mostrar el nombre precio y cantidad de los articulos
 function mostrarCarrito(){
 
-    lista.innerHTML = `ALGO`;
+    lista.innerHTML = " ";
     let contador = 0;
 
     while (contador < carrito.length){
@@ -39,8 +39,8 @@ function mostrarCarrito(){
         let divlista = document.createElement("div");
 
         //modifica html
-        divlista.innerHTML = `El nombre del ${contador+1}º es <strong>${carrito[contador].nombre}</strong><br>
-        El precio del ${contador+1}º es ${carrito[contador].precio}💰<br>
+        divlista.innerHTML = `El nombre del ${contador+1}º es <strong>${carrito[contador+1].nombre}</strong><br>
+        El precio del ${contador+1}º es ${carrito[contador].precio}<br>
         La cantidad del ${contador+1}º es ${carrito[contador].cantidad}<br><hr><hr>`
 
         //añade una clase
@@ -53,7 +53,6 @@ function mostrarCarrito(){
     }
         
 }
-
 
 //Funcion que calcule el importe total
 //producto1.precio * producto1.cantidad + produtcto2.precio * producto2.cantiodad.. ........
@@ -68,7 +67,7 @@ function importeTotal()
         contador ++;
     }
 
-    document.write(`El total es ${total}💰`)
+    document.write(`El total es ${total}`)
 }
 
 cargarBotones();
