6b55809dde1e9182bf844feee0262d75de469588
DAM-Sebasgonz <sebastianmoises2007@gmail.com>
2026-02-10 16:26:39 +0000

    das

---
+++ PRO/TEMA 1/Ejercicios Recuperacion/Ejercicio1.py
@@ -0,0 +1,7 @@
+# Mostrar por pantalla los N primeros números primos. Se pide al usuario el valor de N que
+# debe ser un número entero positivo (debe verificarse).
+# La salida se debe realizar de forma tabulada en la cual cada fila se imprimen 10 números
+# primos.
+# Recuerden que un número primo es aquel que sólo es dividido por la unidad y por el mismo. 
+
+
