7fd11f233baddb246fa48a27ded65f62efc756e4
Sebastian <sebastianmoises2007@gmail.com>
2026-04-22 10:39:08 +0100

    nuevos cambios

---
+++ BAE/UT.7/procemientos.sql
@@ -0,0 +1,5 @@
+-- 1.- Crear un procedimiento que inserte un nuevo estudiante, reciba todos los campos como parámetros y verifique que el DNI no exista antes de la inserción.
+
+delimiter //
+CREATE PROCEDURE
+)
