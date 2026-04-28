bb08391073c0730451b9e46b9aa2c76d6cf76e10
Sebastian <sebastianmoises2007@gmail.com>
2026-04-28 12:33:50 +0100

    teoria tema 6 programacion

---
+++ PRO/TEMA 6/teoria codigos/02-crear_tabla.py
@@ -0,0 +1,30 @@
+import psycopg2
+from psycopg2 import Error
+from getpass import getpass
+
+try:
+    # Conectamos a la BD prueba con el usuario usrpostgre
+    connection = psycopg2.connect(user="usrpostgre",
+                                  password=getpass("Introduzca la contraseña: "),
+                                  host="localhost",
+                                  port="5432",
+                                  database="prueba")
+
+    query = '''CREATE TABLE mobile
+               (ID INT PRIMARY KEY NOT NULL,
+               MODEL TEXT NOT NULL,
+               PRICE REAL);'''
+               
+    # Creamos el cursor y ejecutamos la consulta 
+    cursor = connection.cursor()
+    cursor.execute(query)
+    # consolidamos la modificación de la BD
+    connection.commit()
+    print("La tabla mobile has sido creada satisfactoriamente")
+except (Exception, Error) as error:
+    print("Error...al intentar conectar con PostgreSQL", error)
+else:
+    if (connection):
+        # cursor.close()
+        connection.close()
+        print("La conexión con PostgreSQL está cerrada")
---
+++ PRO/TEMA 6/teoria codigos/07-modificacion-multiple.py
@@ -0,0 +1,32 @@
+import psycopg2
+from psycopg2 import Error
+from getpass import getpass
+
+try:
+    # Conectamos a la BD prueba con el usuario usrpostgre
+    connection = psycopg2.connect(user="usrpostgre",
+                                  password=getpass("Introduzca la contraseña: "),
+                                  host="localhost",
+                                  port="5432",
+                                  database="prueba")
+
+    cursor = connection.cursor()
+    
+    # modificación de datos en la tabla mobile
+
+    query_insertar = """Update mobile set price = %s where id = %s"""
+
+    records = [(750, 4), (950, 5)]
+
+    result = cursor.executemany(query_insertar, records)
+    connection.commit()
+    
+    count = cursor.rowcount
+    print(f"{count} Registros modificados satisfactoriamente")
+except (Exception, Error) as error:
+    print("Error...al intentar conectar con PostgreSQL", error)
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("La conexión con PostgreSQL está cerrada")
